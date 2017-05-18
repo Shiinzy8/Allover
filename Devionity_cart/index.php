065a2dcaba1dbc6718d7705dc0abb7e55bea4bc3
Shiinzy8 <Shiinzy8@gmail.com>
2017-05-19 01:29:37 +0300

    Added ajax

--- Devionity_cart/index.php
+++ Devionity_cart/index.php
@@ -4,6 +4,8 @@ echo'<html>';
 echo'<head>';
     echo'<title>Это ваш новый проект</title>';
     echo'<meta charset="utf-8">';
+    echo '<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>';
+    echo '<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>';
 echo'</head>';
 echo'<body>';
 echo'Hello World!';
@@ -33,15 +35,24 @@ try {
 
     $books = $db->query("SELECT * FROM item");
     foreach ($books as $book) {
-        echo "<b>{$book['name']}</b>  <a href='cart.php?action=add&id={$book['id']}'>Add to cart</a> <br>";
+//        echo "<b>{$book['name']}</b>  <a href='cart.php?action=add&id={$book['id']}'>Add to cart</a> <br>";
+        echo "<b>{$book['name']}</b>  <a href='#' id='add-{$book['id']}'>Add to cart</a> <br>";
     }
 
 } catch (Exception $e) {
     echo $e->getMessage() . ':(';
 }
-echo'<br/>';
-echo'<br/>';
-echo'<a href="cart.php">Show cart</a>';
+?>
+<br/>
+<br/>
+<a href="cart.php">Show cart</a>
+<script>
+    $("a[id|='add']").on('click', function() {
+        id = $(this).attr('id').split('-');
+        id = id[1];
+        $.get('cart.php?action=add&id=' + id, alert('Product ' + id + ' added'));
+    });
+</script>
 
-echo'</body>';
-echo'</html>';
+</body>
+</html>
