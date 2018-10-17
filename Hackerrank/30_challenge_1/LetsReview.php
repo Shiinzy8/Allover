be39ec31295d941cf3fae4e61ff840f2493b2eb7
Shiinzy8 <andrii.arkhyliuk@idapgroup.com>
2018-10-17 13:19:33 +0300

    7 day of challenge

--- Hackerrank/30_challenge_1/LetsReview.php
+++ Hackerrank/30_challenge_1/LetsReview.php
@@ -2,12 +2,8 @@
 $handle = fopen("LetsReview.txt", "r");
 
 $T = fgets($handle);
-while($line = fgets($handle)) {
-    $S = [
-        0 => '',
-        1 => '',
-    ];
-    $line = trim($line);
+while($line = trim(fgets($handle))) {
+    $S = [0 => '',1 => '',];
     for($i = 0; $i < strlen($line); $i++) {
         $S[$i%2] = $S[$i%2] . $line[$i];
     }
