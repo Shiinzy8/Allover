43f9841d40cca627ba65a1640dc397af34e511d7
Andrii Arkhyliuk <Shiinzy8@gmail.com>
2020-03-19 12:48:37 +0200

    testing that not correct results are not returned

--- Udemy/test_with_phpunit/tests/FunctionTest.php
+++ Udemy/test_with_phpunit/tests/FunctionTest.php
@@ -12,4 +12,11 @@ class FunctionTest extends TestCase
         $this->assertEquals(8, add(4, 4));
         $this->assertEquals(9, add(3, 6));
     }
+
+    public function testOperationOfAddReturnNotCorrectSumFailure() 
+    {
+        $this->assertNotEquals(3, add(2, 2));
+        $this->assertNotEquals(5, add(2, 2));
+        $this->assertNotEquals(7, add(2, 2));
+    }
 }
