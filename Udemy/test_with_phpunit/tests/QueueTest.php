64038b89716c7006fb6a316fdd7d0d04e5ca6e2c
Andrii Arkhyliuk <Shiinzy8@gmail.com>
2020-03-19 16:51:51 +0200

    test dependecies

--- Udemy/test_with_phpunit/tests/QueueTest.php
+++ Udemy/test_with_phpunit/tests/QueueTest.php
@@ -9,6 +9,36 @@ class QueueTest extends TestCase
         $queue = new Queue;
 
         $this->assertEquals(0, $queue->getCount());
+
+        return $queue; // we added return to use @depends annotation in other functions
+    }
+
+    /**
+     * @depends testNewQueueIsEmpty
+     */
+    public function testAnItemIsAddedToTheQueueDepended(Queue $queue) 
+    {
+        $first = 1;
+        $queue->push($first);
+
+        $this->assertEquals($first, $queue->pop());
+
+        $second = 'green';
+        $queue->push($second);
+
+        $this->assertEquals($second, $queue->pop());
+    }
+
+    /**
+     * @depends testNewQueueIsEmpty
+     */
+    public function testCountIsWorkingProperlyDepended(Queue $queue) 
+    {
+        $queue->push('green');
+        $queue->push('yellow');
+        $queue->push('blue');
+
+        $this->assertEquals(3, $queue->getCount());
     }
 
     public function testCountIsWorkingProperly() 
