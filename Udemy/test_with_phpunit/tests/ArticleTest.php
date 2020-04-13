c12ce78dc6528f054a56f712e773cb3808d5a66c
Andrii Arkhyliuk <Shiinzy8@gmail.com>
2020-04-13 18:14:21 +0300

    Data provider for a test

--- Udemy/test_with_phpunit/tests/ArticleTest.php
+++ Udemy/test_with_phpunit/tests/ArticleTest.php
@@ -24,31 +24,69 @@ class ArticleTest extends TestCase
         $this->assertSame($this->article->getSlug(), "");
     }
 
-    public function testSlugHasSpacesReplaceByUnderscores()
-    {
-        $this->article->title = "An example article";
+    // public function testSlugHasSpacesReplaceByUnderscores()
+    // {
+    //     $this->article->title = "An example article";
 
-        $this->assertEquals($this->article->getSlug(), "An_example_article");
+    //     $this->assertEquals($this->article->getSlug(), "An_example_article");
+    // }
+
+    // public function testSlugHasWhitespaceReplacedBySingleUnderscore()
+    // {
+    //     $this->article->title = "An       example     \n        article";
+
+    //     $this->assertEquals($this->article->getSlug(), "An_example_article");
+    // }
+
+    // public function testSlugDoesNotStartOrEndWithAnUnderscore()
+    // {
+    //     $this->article->title = "           An       example     \n        article ";
+
+    //     $this->assertEquals($this->article->getSlug(), "An_example_article");
+    // }
+
+    // public function testSlugDoesNotHaveAnyNonWordCharacters()
+    // {
+    //     $this->article->title = "           A$#n       ex%#()#ample     \n        a!@rticle ";
+
+    //     $this->assertEquals($this->article->getSlug(), "An_example_article");   
+    // }
+
+    /**
+     * dataprovider function must be public and with any name
+     * the connection established with annotation @dataProvider
+     */
+    public function titleProvider() 
+    {
+        $slug = "An_example_article";
+
+        // with this result we will see only index of a set with problem
+        // return [
+        //     ["An example article", $slug],
+        //     ["An       example     \n        article", $slug],
+        //     ["           An       example     \n        article ", $slug],
+        //     ["           A$#n       ex%#()#ample     \n        a!@rticle ", $slug],
+        //     ["           A$#n       ex%#()#ample     \n        a!@rticle ", "An_example_article_"],
+        // ];
+
+        // with this result we will see whole names of an index
+        return [
+            "First" => ["An example article", $slug],
+            "Second" => ["An       example     \n        article", $slug],
+            "Third" => ["           An       example     \n        article ", $slug],
+            "Fourth" => ["           A$#n       ex%#()#ample     \n        a!@rticle ", $slug],
+            "Fifth" => ["           A$#n       ex%#()#ample     \n        a!@rticle ", "An_example_article_"],
+        ];
     }
 
-    public function testSlugHasWhitespaceReplacedBySingleUnderscore()
+    /**
+     * @dataProvider titleProvider
+     */
+    public function testSlug($title, $slug)
     {
-        $this->article->title = "An       example     \n        article";
+        $this->article->title = $title;
 
-        $this->assertEquals($this->article->getSlug(), "An_example_article");
-    }
-
-    public function testSlugDoesNotStartOrEndWithAnUnderscore()
-    {
-        $this->article->title = "           An       example     \n        article ";
-
-        $this->assertEquals($this->article->getSlug(), "An_example_article");
-    }
-
-    public function testSlugDoesNotHaveAnyNonWordCharacters()
-    {
-        $this->article->title = "           A$#n       ex%#()#ample     \n        a!@rticle ";
-
-        $this->assertEquals($this->article->getSlug(), "An_example_article");   
+        // as a result we will see 6 assertions
+        $this->assertEquals($this->article->getSlug(), $slug);
     }
 }
