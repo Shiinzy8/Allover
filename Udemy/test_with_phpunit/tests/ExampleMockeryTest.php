e5934d45cbaa66024bba7582b685e679cc3f117b
Andrii Arkhyliuk <Shiinzy8@gmail.com>
2020-04-13 16:29:24 +0300

    extend MockeryTestCase

---
+++ Udemy/test_with_phpunit/tests/ExampleMockeryTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+
+class ExampleMockeryTest extends MockeryTestCase 
+{
+    /**
+     * this method runs after each test method
+     */
+    // public function tearDown(): void 
+    // {
+    //     // we can use it to connect with Mockery, or we can extend our class with dependecry
+    //     // Mocker::close(); 
+    // }
+}
