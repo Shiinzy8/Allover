20321c11caba1d6e22ce7faa935b004d91765e6b
Andrii Arkhyliuk <Shiinzy8@gmail.com>
2020-05-03 21:21:45 +0300

    First and second migration for Article entity

--- Symfony/symfony_workflow_study/src/Entity/Article.php
+++ Symfony/symfony_workflow_study/src/Entity/Article.php
@@ -20,6 +20,26 @@ class Article
     /** @ORM\Column(type="json_array", nullable=true) */
     private $marking;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=100, unique=true)
+     */
+    private $slug;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $publishedAt;
+
     public function getId()
     {
         return $this->id;
@@ -33,5 +53,53 @@ class Article
     public function setMarking($marking)
     {
         $this->marking = $marking;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(?string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getPublishedAt(): ?\DateTimeInterface
+    {
+        return $this->publishedAt;
+    }
+
+    public function setPublishedAt(?\DateTimeInterface $publishedAt): self
+    {
+        $this->publishedAt = $publishedAt;
+
+        return $this;
     }
 }
