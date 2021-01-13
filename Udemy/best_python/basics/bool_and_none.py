e5deff92a5fd296871dd756a4a0f4bf0a9bb4619
Andrii <Shiinzy8@gmail.com>
2021-01-13 19:47:57 +0200

    work with strings

---
+++ Udemy/best_python/basics/bool_and_none.py
@@ -0,0 +1,14 @@
+a = True
+# a = true # this is mistake
+
+b = False
+type(a)
+type(b)
+
+a==b # False
+
+c = 2==2
+type(c) # True
+
+# z # you can't initialize in such way
+z = None
---
+++ Udemy/best_python/basics/string.py
@@ -0,0 +1,36 @@
+x = "Hello, my name is Elias"
+
+# string are immutable
+
+print(len(x))
+print(x[len(x)-1])
+print(x.count('l')) # will counts l in the x string
+
+print(x.capitalize()) # first is capital all others are lowers case
+upper_case = x.upper() # upper all string 
+lower_case = x.lower()
+
+print(upper_case.isupper())
+print(lower_case.islower())
+print(x.isupper())
+print(x.islower())
+
+print(x.find('l'))
+print(x.find('l', 5))
+print(x.find('l', 5, 10))
+print(x.find('m', 8, 15))
+
+print(x.find('my'))
+
+print('123abc'.isalnum())
+print('123abc!'.isalnum())
+
+print('123abc'.isalpha())
+print('abc'.isalpha())
+
+print("   ".isspace())
+print("".isspace())
+
+h = "hello"
+print(h.split('l'))
+print(h.split('e'))
