648218550b4466f46d341888bf452b8def460b79
Andrii <Shiinzy8@gmail.com>
2021-01-14 19:45:27 +0200

    add new cases and write consistency method

--- Python/unittest/1_basic_example/phonebook.py
+++ Python/unittest/1_basic_example/phonebook.py
@@ -1,7 +1,7 @@
 
 class Phonebook:
     def __init__(self):
-        self.entries = {}
+        self.entries = {} # dict
     
     def add(self, name, number):
         self.entries[name] = number
@@ -10,4 +10,27 @@ class Phonebook:
         return self.entries[name]
         
     def is_consistent(self):
+
+        # for duplicates in numbers
+        rev_dict = {}
+        for key, value in self.entries.items():
+            rev_dict.setdefault(value, set()).add(key)
+
+        result = [key for key, values in rev_dict.items() if len(values)>1] # generate list
+        if len(result)>0:
+            return False
+
+        # for empty numbers
+        result = [key for key, values in self.entries.items() if bool(value) is False]
+        if len(result)>0:
+            return False
+
+        # for number is prefix of another number
+        for key, value in self.entries.items():
+            for key_1, value_1 in self.entries.items():
+                if key == key_1:
+                    continue
+                if value_1[:(len(value))] == value:
+                    return False
+
         return True
--- Python/unittest/1_basic_example/test_phonebook.py
+++ Python/unittest/1_basic_example/test_phonebook.py
@@ -19,6 +19,37 @@ class PhoneBookTest(unittest.TestCase):
         with self.assertRaises(KeyError):
             self.phonebook.lookup("missing")
 
-    @unittest.skip("this comment you will see in terminal")
     def test_empty_phonebook_is_consistent(self):
         self.assertTrue(self.phonebook.is_consistent())
+
+    @unittest.skip("bad way to write test, if one is wrong others will skipped")
+    def test_is_consistent(self):
+        self.assertTrue(self.phonebook.is_consistent())
+        self.phonebook.add("Bob", "12345")
+        self.assertTrue(self.phonebook.is_consistent())
+        self.phonebook.add("Mary", "012345")
+        self.assertTrue(self.phonebook.is_consistent())
+        self.phonebook.add("Sue", "12345") # identical to Bob
+        self.assertTrue(self.phonebook.is_consistent())
+        self.phonebook.add("Sue", "123") # prefix of Bob
+
+    def test_phonebook_with_normal_entries_is_consisten(self):
+        self.phonebook.add("Bob", "12345")
+        self.phonebook.add("Marry", "012345")
+        self.assertTrue(self.phonebook.is_consistent())
+
+    def test_phonebook_with_duplicate_entries_is_inconsistent(self):
+        self.phonebook.add("Bob", "12345")
+        self.phonebook.add("Marry", "12345")
+        self.assertFalse(self.phonebook.is_consistent())
+
+    def test_phonebook_with_empty_number_is_inconsistent(self):
+        self.phonebook.add("Bob", "12345")
+        self.phonebook.add("Marry", "")
+        self.assertFalse(self.phonebook.is_consistent())
+
+    def test_phonebook_with_numbers_that_prefix_one_another_is_inconsistent(self):
+        self.phonebook.add("Bob", "12345")
+        self.phonebook.add("Marry", "123")
+        self.assertFalse(self.phonebook.is_consistent())
+    
